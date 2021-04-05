a05a435495fe3f00b8a15ce752ab67265a4f69d5
Michael Kupperman <kupperma@uw.edu>
2021-04-05 01:56:33 -0700

    Basic functionality implemented and MWE

---
+++ examples/Process B.py
@@ -0,0 +1,11 @@
+import  heraut
+
+listener = heraut.Listener(name='process B')
+flag = True
+while flag and listener.listening():
+    # get the message
+    flag, message, label, metadata = listener.get_message()
+    # do something with the message
+    print(message)
+# Shut down the listner.
+listener.end()
---
+++ examples/process A.py
@@ -0,0 +1,8 @@
+import heraut
+
+# Initialize the sender, once per process per sender
+sender = heraut.Sender(name='process A')
+# Send the message
+sender.send(message='hello world', label='', target='process B')
+# Tell the listner no more messages will be coming.
+sender.stop_listener(target='process B')
